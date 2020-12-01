758f92ae8c3b43b4de8e7c2b0298844ad69d4034
agholba <agholba@node0.sid-project.uwmadison744-f20-pg0.wisc.cloudlab.us>
2020-11-30 18:05:20 -0600

    timer

--- test/syntheticSparse.py
+++ test/syntheticSparse.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# TODO 1) Add timers
-# 2) Ring and r-d-h errors
-
 import os, sys
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
@@ -13,6 +10,7 @@ import torch.distributed as dist
 import argparse
 
 from util import _performAllReduce
+from timer import Timer
 
 import scipy.sparse as sparse
 import numpy as np
@@ -40,11 +38,17 @@ t = torch.tensor(sparse.random(size[0], size[1]).A, dtype=torch.float) \
 
 # clone for non-sparse
 t_ns = t.detach().clone()
-print("before t_ns", t_ns)
+# print("before t_ns", t_ns)
 # perform non-sparsified allReduce
+t0 = Timer(0)
+t0.start()
 _performAllReduce(t_ns, args.rank, args.world_size, args.topology, False)
-print("after t_ns", t_ns)
+t0.stop()
+# print("after t_ns", t_ns)
 # perform sparsified allReduce
-print("before t", t)
+# print("before t", t)
+t1 = Timer(1)
+t1.start()
 _performAllReduce(t, args.rank, args.world_size, args.topology, True)
-print("after t", t)
+t1.stop()
+# print("after t", t)
