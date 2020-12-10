11f87331b761048b3c599af29fba133c31d8a767
Siddharth <mohansiddharth96@gmail.com>
2020-12-10 03:05:43 +0000

    Adding size of tensor as argument

--- test/allReduce.py
+++ test/allReduce.py
@@ -16,13 +16,17 @@ parser.add_argument("--rank", type=int, default=0, help="Rank of the process")
 parser.add_argument("--world_size", type=int,default=2, help="No. of processes")
 parser.add_argument("--sparse", help="Test sparse", default=False, action="store_true")
 parser.add_argument("--density", help="Density of tensor", default=0.001)
+parser.add_argument("--size", help="Size of tensor", default=1000)
+
 args = parser.parse_args()
 world_size = args.world_size
 
-dist.init_process_group("gloo", init_method='tcp://10.10.1.1:2345',
+# Modify IP accordingly.
+dist.init_process_group("gloo", init_method='tcp://10.128.0.2:2345',
                         rank=args.rank, world_size=world_size)
 
-t = torch.tensor(sparse.random(1000, 10000, density=args.density).A, dtype=torch.float) \
+size = int(args.size)
+t = torch.tensor(sparse.random(size, size, density=args.density).A, dtype=torch.float) \
     * np.random.randint(10, 100)
 
 t_ns = t.detach().clone()
@@ -46,6 +50,7 @@ for i in range(1, len(gatherList)):
 res /= world_size
 e1 = t1.stop()
 
+
 # AllGather sparse
 t2 = Timer(2)
 t2.start()
@@ -56,7 +61,7 @@ values = t_ns1.values()
 
 nnz_tensor = torch.tensor(nnz)
 dist.all_reduce(nnz_tensor, op=dist.ReduceOp.SUM)
-gatherListIndices = [torch.zeros([2, nnz_tensor.item()], dtype=torch.long) for _ in range(world_size)]
+gatherListIndices = [torch.zeros([2, nnz], dtype=torch.long) for _ in range(world_size)]
 dist.all_gather(gatherListIndices, indices)
 gatherListValues = [torch.zeros([nnz], dtype=torch.float) for _ in range(world_size)]
 dist.all_gather(gatherListValues, values)
