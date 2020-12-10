95220caa12adc984eef1b23fcdabe239360458f6
smohan25 <smohan25@node0.proj-cluster.uwmadison744-f20-pg0.wisc.cloudlab.us>
2020-12-09 18:41:31 -0600

    Add sparse allgather

--- test/allReduce.py
+++ test/allReduce.py
@@ -15,19 +15,20 @@ parser = argparse.ArgumentParser(description="Basic Tree AllReduce")
 parser.add_argument("--rank", type=int, default=0, help="Rank of the process")
 parser.add_argument("--world_size", type=int,default=2, help="No. of processes")
 parser.add_argument("--sparse", help="Test sparse", default=False, action="store_true")
-
+parser.add_argument("--density", help="Density of tensor", default=0.001)
 args = parser.parse_args()
 world_size = args.world_size
 
 dist.init_process_group("gloo", init_method='tcp://10.10.1.1:2345',
                         rank=args.rank, world_size=world_size)
 
-t = torch.tensor(sparse.random(1000, 1000).A, dtype=torch.float) \
+t = torch.tensor(sparse.random(1000, 10000, density=args.density).A, dtype=torch.float) \
     * np.random.randint(10, 100)
 
 t_ns = t.detach().clone()
+t_ns1 = t.detach().clone()
 
-#AllReduce
+# AllReduce
 t0 = Timer(0)
 t0.start()
 dist.all_reduce(t, op=dist.ReduceOp.SUM)
@@ -45,4 +46,38 @@ for i in range(1, len(gatherList)):
 res /= world_size
 e1 = t1.stop()
 
-print(f"{e0:0.4f}, {e1:0.4f}")
+# AllGather sparse
+t2 = Timer(2)
+t2.start()
+t_ns1 = t_ns1.to_sparse()#.coalesce()
+nnz = t_ns1._nnz()
+indices = t_ns1.indices()
+values = t_ns1.values()
+
+nnz_tensor = torch.tensor(nnz)
+dist.all_reduce(nnz_tensor, op=dist.ReduceOp.SUM)
+gatherListIndices = [torch.zeros([2, nnz_tensor.item()], dtype=torch.long) for _ in range(world_size)]
+dist.all_gather(gatherListIndices, indices)
+gatherListValues = [torch.zeros([nnz], dtype=torch.float) for _ in range(world_size)]
+dist.all_gather(gatherListValues, values)
+
+dic = {}
+for i, v in zip(gatherListIndices, gatherListValues):
+    for x, y, z in zip(i[0], i[1], v):
+        x, y, z = x.item(), y.item(), z.item()
+        if (x, y) in dic:
+            dic[(x, y)] += z
+        else:
+            dic[(x, y)] = z
+new_indices = [[], []]
+new_values = []
+for k, v in dic.items():
+    new_indices[0].append(k[0])
+    new_indices[1].append(k[1])
+    new_values.append(v)
+#print(new_indices)
+sparseTensor = torch.sparse.LongTensor(torch.LongTensor(new_indices), torch.FloatTensor(new_values), t.size())
+e2 = t2.stop()
+
+print("Allreduce, Allgather, Allgather-sparse")
+print(f"{e0:0.4f}, {e1:0.4f}, {e2:0.4f}")
